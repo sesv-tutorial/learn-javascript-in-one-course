5d8f6baa4e7a79db5067c772bfca5141dcf6eaee
SESV Tutorial <coded.fire@icloud.com>
2020-03-19 10:36:39 -0700

    add lec1

---
+++ lecture-1/square.js
@@ -0,0 +1,10 @@
+// function to square up any number (the process #2)
+function square(x) {
+  return x*x
+}
+
+const input = 2 // #1 input - information we already have
+const output = square(input) // #3 output - the desired information we want
+
+// print out output to terminal
+console.log('output:', output) // output: 4
---
+++ lecture-1/square2.js
@@ -0,0 +1,9 @@
+// my thought: I want to square up any number
+// express my thought in Javascript language
+function square(x) {
+  return x*x
+}
+
+// now the computer can do the square job for me whenever I tell it to
+console.log('output:', square(2)) // output: 4
+console.log('output:', square(3)) // output: 9
